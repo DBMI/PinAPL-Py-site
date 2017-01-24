cf35eec0314136d83768e29f2492ca6eea5d1bfb
Tyler <tylerbath@gmail.com>
2017-01-23 19:14:57 -0800

    Had a typo

--- app/Scripts/setup.sh
+++ app/Scripts/setup.sh
@@ -5,8 +5,13 @@ scriptPath=$(cd -P -- "$(dirname -- "$0")" && pwd -P)
 cd $scriptPath/..
 sitePath=$(pwd)
 
+sudo apt -qq install npm
+sudo npm install -g forever
+
+sudo apt -qq install supervisor
+
 # Setup and start queue
-ln sudo ln -s $sitePath/setup/pinapl-worker.conf /etc/supervisor/conf.d/pinapl-worker.conf
+sudo ln -s $sitePath/setup/pinapl-worker.conf /etc/supervisor/conf.d/pinapl-worker.conf
 $scriptPath/startSupervisor.sh
 
 # Start Kotrans server
