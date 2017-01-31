afd34a98b7291d9a7f9b9b97c80dd0f1e1363c52
Tyler <tylerbath@gmail.com>
2017-01-31 12:00:40 -0800

    Cleaned up topbar. Added contacts page

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,43 @@
+@extends('layouts.master')
+@section('content')
+<div id="content">
+	<div class="row">
+		<div class="twelve columns">
+			<h1>Contact Us</h1>
+
+			<hr> 
+			<h4>PinAPL-py</h4>
+			<p>
+			</p><ul>
+			<li>Olivier Harismendy</li>              
+			<li>Philip Spahn</li>              
+			<li>Tyler Bath </li>              
+			<li>Lucila Ohno-Machado</li>              
+		</ul> 
+		<p></p>
+
+		<hr>
+		<h4>Division of Biomedical Informatics</h4>
+		<p> 
+			The <a href="http://dbmi.ucsd.edu" target="_blank">Division of Biomedical Informatics (DBMI)</a>
+			at University of California-San Diego(UCSD)  was established in the summer of 2009. 
+			It is the first academic unit in biomedical informatics in the UC system. 
+			Our activities involve research, teaching, and multi-disciplinary collaboration. 
+			DBMI members have diverse backgrounds in medicine, biology, nursing, computer science,
+			mathematics, statistics, engineering, and business. Our division is based in La Jolla, California,
+			but is connected to collaborators all around the world. 
+		</p>
+
+
+		
+		<hr>
+		<h4>Questions and Comments</h4>
+		<p>
+			<a href="mailto:pspahn@eng.ucsd.edu">pspahn@eng.ucsd.edu</a>
+		</p>
+
+
+	</div>
+</div>
+</div>
+@stop
--- resources/views/layouts/topbar.blade.php
+++ resources/views/layouts/topbar.blade.php
@@ -1,14 +1,12 @@
 <div class="top-bar">
   <div class="top-bar-left">
     <ul class="menu">
-      <li class="menu-text">PinAPL-Py</li>
+      <li class="menu-text"><a href="/">PinAPL-Py</a></li>
     </ul>
   </div>
   <div class="top-bar-right">
     <ul class="menu">
-      <li><a href="/gettingstarted">Quick Start</a></li>
-      <li><a href="/manual">Help</a></li>
-      <li><a href="/data">Sample Data</a></li>
+      <li><a href="/sample-data">Sample Data</a></li>
       <li><a href="/contact">Contact Us</a></li>
     </ul>
   </div>
