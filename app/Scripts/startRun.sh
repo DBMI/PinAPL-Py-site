fa5cf0c3e372e99566d8c89a5b3cbdca5d4ca861
Tyler <tylerbath@gmail.com>
2017-03-03 11:00:03 -0800

    Using docker version 2.15.0

--- app/Scripts/startRun.sh
+++ app/Scripts/startRun.sh
@@ -4,5 +4,5 @@ RUN_DIR=$1
 echo $RUN_DIR
 echo "Start Run Script Started"
 echo "running" > $RUN_DIR/status.log
-docker run --rm -v $RUN_DIR/workingDir:/workingdir oncogx/pinaplpy_docker:beta_v2.1.0 /bin/bash -c "python -u /opt/PinAPL-Py/Scripts/PinAPL.py > /workingdir/output.log 2>&1 && chown -R www-data:www-data /workingdir"
+docker run --rm -v $RUN_DIR/workingDir:/workingdir oncogx/pinaplpy_docker:beta_v2.15.0 /bin/bash -c "python -u /opt/PinAPL-Py/Scripts/PinAPL.py > /workingdir/output.log 2>&1 && chown -R www-data:www-data /workingdir"
 echo "finished" > $RUN_DIR/status.log
