1e3fd2a8b7bf1672f32abbf8ede58127a57226d2
Tyler <tylerbath@gmail.com>
2017-03-08 12:30:59 -0800

    rankings only display tsv files

--- resources/views/results/gene_rankings_component.blade.php
+++ resources/views/results/gene_rankings_component.blade.php
@@ -1,5 +1,5 @@
 <?php
-$files = \File::glob(storage_path("runs/$runHash/workingDir/Analysis/Gene_Rankings/$prefix*"));
+$files = \File::glob(storage_path("runs/$runHash/workingDir/Analysis/Gene_Rankings/$prefix*.tsv"));
 ?>
 @foreach ($files as $file)
 {!! svToHTML($file, "\t") !!}
--- resources/views/results/sgrna_rankings_component.blade.php
+++ resources/views/results/sgrna_rankings_component.blade.php
@@ -1,5 +1,5 @@
 <?php
-$files = \File::glob(storage_path("runs/$runHash/workingDir/Analysis/sgRNA_Rankings/$prefix*"));
+$files = \File::glob(storage_path("runs/$runHash/workingDir/Analysis/sgRNA_Rankings/$prefix*.tsv"));
 ?>
 @foreach ($files as $file)
 {!! svToHTML($file, "\t") !!}
