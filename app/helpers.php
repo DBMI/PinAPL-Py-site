a8faed1f4508c8cc4f23aa50a37210015f3dda4a
Tyler <tylerbath@gmail.com>
2017-03-19 20:44:32 -0700

    More download

--- app/helpers.php
+++ app/helpers.php
@@ -10,6 +10,22 @@ function download($file, $name = null, array $headers = array(), $disposition = 
 	return $response;
 }
 
+function return_bytes($val)
+{
+  $val = trim($val);
+  $last = strtolower($val[strlen($val) - 1]);
+  switch ($last) {
+  // The 'G' modifier is available since PHP 5.1.0  
+  case 'g':
+    $val*= 1024;
+  case 'm':
+    $val*= 1024;
+  case 'k':
+    $val*= 1024;
+  }
+  return $val;
+}
+
 function makeDir($path, $mode = 0755, $recursive = false, $force = false)
 {
 	$umask_old = umask(0);
