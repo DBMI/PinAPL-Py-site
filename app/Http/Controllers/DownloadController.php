0a9aa847efc6092b18cf894f3c2bf08a95ea1491
Tyler Bath <tylerbath@gmail.com>
2017-03-19 20:17:32 -0700

    Added new method

--- app/Http/Controllers/DownloadController.php
+++ app/Http/Controllers/DownloadController.php
@@ -34,7 +34,70 @@ public function laravel_zip_header()
 
 	return response()->download($this->filepath, $this->filename, $headers);
 }
-public function whywhywhy (){
-	return "fc=";
+function can_haz_xsendfile()
+{
+  // This will return false if PHP is not loaded as a module (i.e. uses cgi)
+  return in_array('mod_xsendfile', apache_get_modules());
 }
+
+function send_download_package_file()
+{
+  $realpath = $this->filepath;
+  if (file_exists($realpath)) {
+    // Fetching File
+    $mtime = ($mtime = filemtime($realpath)) ? $mtime : gmtime();
+    $size = intval(sprintf("%u", filesize($realpath)));
+    header("Content-type: application/force-download");
+    header('Content-Type: application/octet-stream');
+    if (strstr($_SERVER["HTTP_USER_AGENT"], "MSIE") != false) {
+      header("Content-Disposition: attachment; filename=" . urlencode($this->filename) . '; modification-date="' . date('r', $mtime) . '";');
+    } else {
+      header("Content-Disposition: attachment; filename=\"" . $this->filename . '"; modification-date="' . date('r', $mtime) . '";');
+    }
+    if (in_array('mod_xsendfile', apache_get_modules())) {
+      // Sending file via mod_xsendfile
+      header("X-Sendfile: " . $this->filepath);
+    } else {
+      // Sending file directly via script
+      if (intval($size + 1) > return_bytes(ini_get('memory_limit')) && intval($size * 1.5) <= 1073741824) { //Not higher than 1GB
+        // Setting memory limit
+        ini_set('memory_limit', intval($size * 1.5));
+      }
+      @apache_setenv('no-gzip', 1);
+      @ini_set('zlib.output_compression', 0);
+      header("Content-Length: " . $size);
+      // Set the time limit based on an average D/L speed of 50kb/sec
+      set_time_limit(min(7200, // No more than 120 minutes (this is really bad, but...)
+      ($size > 0) ? intval($size / 51200) + 60 // 1 minute more than what it should take to D/L at 50kb/sec
+       : 1 // Minimum of 1 second in case size is found to be 0
+      ));
+      $chunksize = 1 * (1024 * 1024); // how many megabytes to read at a time
+      if ($size > $chunksize) {
+        // Chunking file for download
+        $handle = fopen($realpath, 'rb');
+        $buffer = '';
+        while (!feof($handle)) {
+          $buffer = fread($handle, $chunksize);
+          echo $buffer;
+          ob_flush();
+          flush();
+        }
+        fclose($handle);
+      } else {
+        // Streaming whole file for download
+        readfile($realpath);
+      }
+    }
+    return true;
+  } else {
+    // File not found! Throw error here...
+  }
+  return false;
+}
+
+
+
+
+
+
 }
