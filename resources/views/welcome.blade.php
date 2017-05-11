296fd2ab447a73096e20cad2d7c89ddcda80e36a
Tyler <tylerbath@gmail.com>
2017-05-11 16:42:01 -0700

    Added help image to welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -12,34 +12,34 @@
 		<div class="row">
 			Please enter a valid e-mail address and a name for your analysis run below
 		</div>
-		</span>
-	</div>
-</div>
-<div class="row">
-	<div class="small-12 columns">
-		<div class="callout">
-			<form action="/createRun" method="post" class="custom" enctype="multipart/form-data">
-				<input type="hidden" name="_token" value="{{ csrf_token() }}">
-				<div class="row">
-					<div class="medium-6 columns">
-						<label for="email"><span class="has-tip tip-top whiteTxt" title="The e-mail address we should send your results to">E-mail Address</span></label>
-						<input id="email" name="email" type="text" required />
-					</div>
-					<div class="medium-6 columns">
-						<label for="name"><span class="has-tip tip-top whiteTxt" title="Please give this data a name">Analysis Run Name</span></label>
-						<input id="name" name="name" type="text"/>
-					</div>
-				</div>
+		<div class="row collapse">
+			<div class="small-12 columns">
+				<div class="callout">
+					<form action="/createRun" method="post" class="custom" enctype="multipart/form-data">
+						<input type="hidden" name="_token" value="{{ csrf_token() }}">
+						<div class="row">
+							<div class="medium-6 columns">
+								<label for="email"><span class="has-tip tip-top whiteTxt" title="The e-mail address we should send your results to">E-mail Address</span></label>
+								<input id="email" name="email" type="text" required />
+							</div>
+							<div class="medium-6 columns">
+								<label for="name"><span class="has-tip tip-top whiteTxt" title="Please give this data a name">Analysis Run Name</span></label>
+								<input id="name" name="name" type="text"/>
+							</div>
+						</div>
 
-				<div class="row align-right">
-					<div class="shrink columns">
-						<input type="submit" id="runSubmitButton" class="medium success button expand"/>
-					</div>
+						<div class="row align-right">
+							<div class="shrink columns">
+								<input type="submit" id="runSubmitButton" class="medium success button expand"/>
+							</div>
+						</div>
+					</form>
 				</div>
-			</form>
+			</div>
 		</div>
 	</div>
 </div>
+<img id="how-to-img" src="/img/howTo.png" >
 
 <div class="row footer align-center">
 	<div class="shrink columns">
@@ -49,16 +49,25 @@
 @stop
 @section('customCSS')
 	<style type="text/css">
-		#welcome-CSS {
-			height: 210px;
-		}
 		#welcome-text{
-			height: 210px;
+			height: 326px;
 			justify-content: space-between;
 	    flex-direction: column;
 	    display: flex;
 	    padding-top: 1em;
 	    padding-bottom: 1em;
+		}
+		#how-to-img {
+			margin-left: 2em;
+		}
+		.row {
+			max-width: 82.5rem;
+		}
+		.callout input[type='submit'] {
+			margin-bottom: 0;
+		}
+		.callout {
+			margin-bottom: 0;
 		}
 	</style>
 @stop
