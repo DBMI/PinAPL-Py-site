9f6a6fffa86ecb723b4b43e6110d0a2e8e72dcd1
Tyler <tylerbath@gmail.com>
2017-07-03 19:16:28 -0700

    Replicate correlation align center

--- resources/views/results/replicate_correlation_component.blade.php
+++ resources/views/results/replicate_correlation_component.blade.php
@@ -2,5 +2,9 @@
 $files = \File::glob(storage_path("runs/$runHash/workingDir/Analysis/Replicate_Correlation/*.png"));
 ?>
 @foreach ($files as $file)
-<img src='{{"/run-images?path=".urlencode("/$runHash/workingDir/Analysis/Replicate_Correlation/".basename($file))}}'>
+	<div class="row align-center">
+		<div class="column shrink">
+			<img src='{{"/run-images?path=".urlencode("/$runHash/workingDir/Analysis/Replicate_Correlation/".basename($file))}}'>
+		</div>
+	</div>
 @endforeach
