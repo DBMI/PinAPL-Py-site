44b7ce162ad2c085e06f707230e3735a6789d2fc
Tyler <tylerbath@gmail.com>
2017-07-08 19:47:45 -0700

    Text field hidden on control select

--- resources/views/files.blade.php
+++ resources/views/files.blade.php
@@ -13,7 +13,7 @@
 </div>
 @stop
 @section('content')
-<form action="/configure-files/{{ $hash }}" method="POST">
+<form action="/configure-files/{{ $hash }}" method="POST" id="configure-files">
 <div class="row">
 	<div class="column medium-5"><strong>Filename</strong></div>
 	<div class="column medium-1"><strong title="Is this a control file?">Control</strong></div>
@@ -21,13 +21,16 @@
 </div>
 <input type="hidden" name="_token" value="{{ csrf_token() }}">
 @foreach ($files as $file)
-	<?php $file = basename($file); ?>
-	<div class="row" id="{{ $file }}">
+	<?php 
+		$file = basename($file); 
+		$id = preg_replace("/[^a-zA-Z0-9_-]/", "_", $file);
+	?>
+	<div class="row align-middle" id="{{ $file }}">
 		<div class="column medium-5 file-name"><span title="{{ $file }}">{{ $file }}</span></div>
 		<div class="column medium-1 text-center">
-			<input type="checkbox" value="control" name="group-{{ $file }}" onclick="controlClick('{{ $file }}')">
+			<input type="checkbox" value="control" id="group-{{ $id }}" name="group-{{ $file }}" onclick="controlClick('{{ $id }}')">
 		</div>
-		<div class="column medium-5"><input type="text" name="rename-{{$file}}" placeholder="Condition Name"></div>
+		<div class="column medium-5"><input type="text" id="rename-{{ $id }}" name="rename-{{$file}}" placeholder="Condition Name"></div>
 	</div>
 @endforeach
 <div class="row align-right">
@@ -41,13 +44,23 @@
 		overflow: hidden;
 		text-overflow: ellipsis;
 	}
+	#configure-files input{
+		margin-bottom: 0;
+	}
+	#configure-files input[type=text]{
+		height: initial;
+		padding: .3rem;
+	}
+	#configure-files .row{
+		margin-bottom: .5rem;
+	}
 </style>
 @stop
 @section('customScripts')
 <script type="text/javascript">
 	function controlClick(file) {
-		checked = true;
-		$('#rename-'+file).toggle(!checked)
+		checked = $('#group-'+file).is(':checked');
+		$('#rename-'+file).css('visibility', this.checked ? 'hidden' : 'visible');
 	}
 </script>
 @stop
