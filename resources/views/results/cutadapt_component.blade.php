d6f759be5d7d46b3fa7fcd0fbdd2444dc0960302
Tyler <tylerbath@gmail.com>
2017-07-09 18:28:58 -0700

    Forgot to read file

--- resources/views/results/cutadapt_component.blade.php
+++ resources/views/results/cutadapt_component.blade.php
@@ -1,6 +1,6 @@
 @php
 	$file = \File::glob(storage_path("runs/$runHash/workingDir/Analysis/Read_Trimming/cutadapt_$fileName*.txt")) ;
-	$file = array_shift($file);
+	$file = \File::get(array_shift($file));
 @endphp
 <div class="row align-center">
 	<div class="column shrink">
