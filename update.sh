785353ff4e11efa1af0e47ff13548630cd25bc46
Tyler <tylerbath@gmail.com>
2017-07-15 23:43:16 -0700

    Made update script execute in is location directory

--- update.sh
+++ update.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+
+# Change directory to where the script is stored, not run from
+scriptPath=$(cd -P -- "$(dirname -- "$0")" && pwd -P)
+cd $scriptPath
+
 git pull
 php artisan migrate
 php artisan config:cache
