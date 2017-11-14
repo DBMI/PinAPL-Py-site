c0a8c4b7bbbeeb5b801010a189e5f8b707f09133
Tyler <tylerbath@gmail.com>
2017-11-13 17:09:08 -0800

    Flushed out readme and setup.sh

--- readme.md
+++ readme.md
@@ -1,40 +1,21 @@
-<p align="center"><img src="https://laravel.com/assets/img/components/logo-laravel.svg"></p>
+<p align="center"><img src="https://github.com/DBMI/PinAPL-Py-site/blob/master/public/img/logo_with_name.png"></p>
 
-<p align="center">
-<a href="https://travis-ci.org/laravel/framework"><img src="https://travis-ci.org/laravel/framework.svg" alt="Build Status"></a>
-<a href="https://packagist.org/packages/laravel/framework"><img src="https://poser.pugx.org/laravel/framework/d/total.svg" alt="Total Downloads"></a>
-<a href="https://packagist.org/packages/laravel/framework"><img src="https://poser.pugx.org/laravel/framework/v/stable.svg" alt="Latest Stable Version"></a>
-<a href="https://packagist.org/packages/laravel/framework"><img src="https://poser.pugx.org/laravel/framework/license.svg" alt="License"></a>
-</p>
+## About
 
-## About Laravel
+This a a website built for the CRISPR CAS9 pipeline <a href="https://github.com/LewisLabUCSD/PinAPL-Py">PinAPL-Py</a> written by Philipp Spahn.
 
-Laravel is a web application framework with expressive, elegant syntax. We believe development must be an enjoyable, creative experience to be truly fulfilling. Laravel attempts to take the pain out of development by easing common tasks used in the majority of web projects, such as:
+It is written mainly in PHP using the php framework <a href="https://laravel.com/">Laravel</a>
 
-- [Simple, fast routing engine](https://laravel.com/docs/routing).
-- [Powerful dependency injection container](https://laravel.com/docs/container).
-- Multiple back-ends for [session](https://laravel.com/docs/session) and [cache](https://laravel.com/docs/cache) storage.
-- Expressive, intuitive [database ORM](https://laravel.com/docs/eloquent).
-- Database agnostic [schema migrations](https://laravel.com/docs/migrations).
-- [Robust background job processing](https://laravel.com/docs/queues).
-- [Real-time event broadcasting](https://laravel.com/docs/broadcasting).
+We use a custom file upload server written in nodeJS which allows for the upload of large files. The larges we tested was 300GB
 
-Laravel is accessible, yet powerful, providing tools needed for large, robust applications. A superb combination of simplicity, elegance, and innovation give you tools you need to build any application with which you are tasked.
+## Installation & Setup
+Clone this repository to where you would like to host the site. E.g. /var/www/
 
-## Learning Laravel
+The site comes with a setup script included in setup/setup.sh. This script is more of a guideline, and is written for ubuntu 16.04. It is not thoroughly tested and should not be used for production machines. 
+``` sudo bash setup/setup.sh ```
 
-Laravel has the most extensive and thorough documentation and video tutorial library of any modern web application framework. The [Laravel documentation](https://laravel.com/docs) is thorough, complete, and makes it a breeze to get started learning the framework.
-
-If you're not in the mood to read, [Laracasts](https://laracasts.com) contains over 900 video tutorials on a range of topics including Laravel, modern PHP, unit testing, JavaScript, and more. Boost the skill level of yourself and your entire team by digging into our comprehensive video library.
-
-## Contributing
-
-Thank you for considering contributing to the Laravel framework! The contribution guide can be found in the [Laravel documentation](http://laravel.com/docs/contributions).
-
-## Security Vulnerabilities
-
-If you discover a security vulnerability within Laravel, please send an e-mail to Taylor Otwell at taylor@laravel.com. All security vulnerabilities will be promptly addressed.
+After installation and setup open the .env file and fill in any empty variables, and change any that are incorrect. 
 
 ## License
 
-The Laravel framework is open-sourced software licensed under the [MIT license](http://opensource.org/licenses/MIT).
+This code is licensed under the [MIT license](http://opensource.org/licenses/MIT).
--- setup/setup.sh
+++ setup/setup.sh
@@ -1,24 +1,57 @@
 
 #!/bin/bash
 
+echo -e "\e[1m"
+echo "Progress updates and prompts from this script will be in this color"
+echo "This script is for development enviroments only. Please use as a guide for manual installation on production enviroments"
+echo "We are not liable for any problems this script may cause"
+read -r -p "Would you like to continue? [y/N] " response
+case "$response" in
+    [yY][eE][sS]|[yY]) 
+        continue
+        ;;
+    *)
+		echo "Exiting script"
+        exit 1
+        ;;
+esac
+echo -e "\e[0m"
+
+
 # Change directory to where the script is stored, not run from
 scriptPath=$(cd -P -- "$(dirname -- "$0")" && pwd -P)
 cd $scriptPath/..
 sitePath=$(pwd)
 
+siteName="PinAPL-py" 
+siteSafeName="pinaplpy" # Should be all lowercase and no symbols
+
 
 echo -e "\e[1mInstalling required packages\e[0m"
-
+sudo apt -qq update
+sudo apt -qq install curl
 sudo apt -qq install npm
 sudo apt -qq install zip
 sudo apt -qq install php-zip
+sudo phpenmod zip
 sudo apt -qq install php-xml
 sudo apt -qq install php-dom
 sudo apt -qq install php-mbstring
-sudo phpenmod zip
 sudo npm install -g forever
-
 sudo apt -qq install supervisor
+
+# Install Docker 
+sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+curl -fsSL get.docker.com -o get-docker.sh
+sudo sh get-docker.sh
+
 
 # ################################
 # Install Composer
@@ -50,16 +83,16 @@ else
 	apt-get -qq install mysql-server > /dev/null
 fi
 service mysql start
-echo "Please provide a password for the PinAPL-py user"
+echo "Please provide a password for the $siteName user"
 read -s mysqlPass
 if [[ -n $mysqlRoot ]]; then
 	passFlag="-p$mysqlRoot"
 else
 	passFlag=""
 fi
-mysql -u root $passFlag -e "CREATE DATABASE pinaplpy"
-mysql -u root $passFlag -e "CREATE USER 'pinaplpy'@'localhost' IDENTIFIED BY '$mysqlPass'"
-mysql -u root $passFlag -e "Grant ALL ON pinaplpy.* TO 'pinaplpy'@'localhost'"
+mysql -u root $passFlag -e "CREATE DATABASE $siteSafeName"
+mysql -u root $passFlag -e "CREATE USER '$siteSafeName'@'localhost' IDENTIFIED BY '$mysqlPass'"
+mysql -u root $passFlag -e "Grant ALL ON $siteSafeName.* TO '$siteSafeName'@'localhost'"
 mysql -u root $passFlag -e "FLUSH PRIVILEGES"
 
 
@@ -68,7 +101,40 @@ sed -i "s/DB_PASSWORD=secret/DB_PASSWORD=${mysqlPass}/g" .env
 echo -e "\e[1mSetting up Database\e[0m"
 php artisan migrate
 
+# ################################
+# Setup Apache
+# ################################
 echo -e "\e[1mSetting up Apache\e[0m"
+# Setting up virtual host
+virtualHost="/etc/apache2/sites-available/$siteSafeName.conf"
+sudo cp $sitePath/setup/$siteSafeName.conf "$virtualHost"
+# Set up virtualhost
+
+sudo cat > "$virtualHost" <<EOT 
+<VirtualHost ${siteSafeName}:80>
+	DocumentRoot ${sitePath}
+	ServerName ${siteSafeName}
+	ServerAlias ${siteSafeName
+
+	<Directory ${sitePath}>
+		AllowOverride All
+	</Directory>
+	
+	ErrorLog ${sitePath}/storage/logs/apache_error.log
+	CustomLog ${sitePath}/storage/logs/apache_access.log combined
+	
+	XSendFile on
+	XSendFilePath /var/www
+</VirtualHost>
+# vim: syntax=apache ts=4 sw=4 sts=4 sr noet
+EOT
+
+
+sudo a2ensite $siteSafeName.conf
+
+
+# Add flightdeck localhost to /etc/hosts
+sudo echo -e "\n127.0.0.1 $siteSafeName" >> /etc/hosts
 a2enmod rewrite
 service apache2 restart
 
@@ -76,17 +142,30 @@ service apache2 restart
 echo -e "\e[1mMoving Files\e[0m"
 cp .env.example .env
 
-# Setup and start queue
-sudo ln -s $sitePath/setup/pinapl-worker.conf /etc/supervisor/conf.d/pinapl-worker.conf
-sudo ln -s $sitePath/setup/pinapl-worker-monitor.conf /etc/supervisor/conf.d/pinapl-worker-monitor.conf
-sudo ln -s $sitePath/setup/pinapl-worker-start-run.conf /etc/supervisor/conf.d/pinapl-worker-start-run.conf
+# Symlink all supervisor files to the supervisor directory
+sudo ln -s $sitePath/setup/supervisor/* /etc/supervisor/conf.d/
 
 # ################################
 # Change ownership and permisions
 # ################################
 echo -e "\e[1mChanging File Permissions for $sitePath\e[0m"
-chown -R www-data:www-data $sitePath
-chmod -R 775 $sitePath
+sudo chown -R www-data:www-data $sitePath
+sudo chmod -R 775 $sitePath
+
+# Adding the docker group
+echo -e "\e[1mAdding Current user to the docker and www-data groups\e[0m"
+sudo groupadd docker
+sudo usermod -aG docker $USER 
+sudo usermod -aG www-data $USER
+
+# Pulling Docker image
+echo -e "\e[1mPuling Docker Image\e[0m"
+regex="DOCKER_IMAGE=(.+)"
+OUTPUT=$(cat .env)
+[[ $OUTPUT =~ $regex ]]
+DOCKER_IMAGE=${BASH_REMATCH[1]}
+
+docker pull "$DOCKER_IMAGE"
 
 # Start Supervisor
 echo -e "\e[1mStarting Supervisor (Queue Management)\e[0m"
@@ -96,7 +175,12 @@ $scriptPath/startSupervisor.sh
 echo -e "\e[1mStarting KoTrans (File Upload Server)\e[0m"
 $scriptPath/startKoTrans.sh
 
+echo -e "\e[1mInstall Complete. You may need to reboot\e[0m"
+
 echo "\e[1mPlease ensure the system has enough ram. We reccomend a few GB more then the largest file you plan to run.\e[0m"
 echo "\e[1mPlease edit your php.ini file and change max_filesize to allow 1G uploads.\e[0m"
 echo "\e[1mPlease install docker. Visit https://docs.docker.com/engine/installation/ for instructions.\e[0m"
+
+echo -e "\e[1mPlease ensure that the paths are correct in /etc/apache2/sites-available/$siteSafeName.conf\e[0m"
+echo -e "\e[1mIt is setup for localhost use. If you would like to enable external access please replace $siteSafeName:80 with *:80, or with your url:port \e[0m"
 
