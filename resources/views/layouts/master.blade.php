9d34b980b4d86a5e168aa14bf95c69e68cf223c0
Tyler <tylerbath@gmail.com>
2018-01-17 20:08:36 -0800

    Added google analytics

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -10,6 +10,18 @@
     <link rel="stylesheet" href="/css/app.css">
     @yield('customCSS')
     <script src="/js/vendor/modernizr.js"></script>
+    @if (!App::isLocal())
+      <!-- Global site tag (gtag.js) - Google Analytics -->
+      <script async src="https://www.googletagmanager.com/gtag/js?id=UA-64425044-2"></script>
+      <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'UA-64425044-2');
+      </script>
+
+    @endif
   </head>
   <body>
     @include('layouts.topbar')
