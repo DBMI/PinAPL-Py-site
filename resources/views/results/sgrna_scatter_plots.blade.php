f6d726c04506bdde476a6dc723d66067934d9af2
DarrenChangSD <djc035@ucsd.edu>
2019-08-28 13:33:31 -0700

    new sgrna scatterplots tabs

---
+++ resources/views/results/sgrna_scatter_plots.blade.php
@@ -0,0 +1,7 @@
+<?php
+$sections = [
+	"readcount_scatterplots"  => "Treatment vs Control",
+	"replicate_correlation"	=> "Replicate Correlation",
+];
+?>
+@extends('layouts.results_section_tabs', ['sections' => $sections, "selfName"=>"sgrna-scatter-plots"])
