8514d09270ea2c32545265de3db2cfecf7549a88
DarrenChangSD <djc035@ucsd.edu>
2019-08-28 13:35:10 -0700

    new volcano plots page

---
+++ resources/views/results/volcano_plots_component.blade.php
@@ -0,0 +1,1 @@
+<img id="{{ $prefix }}-volcano-plot" src='{{"/run-images?path=".urlencode("/$runHash/workingDir/Analysis/02_sgRNA-Ranking_Results/sgRNA_VolcanoPlots/$prefix"."_sgRNA_volcano.png" )}}'>
