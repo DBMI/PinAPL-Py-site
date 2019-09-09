a28b3cb1c6f04fa07b3348464d10ccab2da6b516
DarrenChangSD <djc035@ucsd.edu>
2019-09-09 12:05:28 -0700

    volcano plot added extraData for nonT pre checked

--- resources/views/results/volcano_plots_component.blade.php
+++ resources/views/results/volcano_plots_component.blade.php
@@ -1,1 +1,5 @@
-<img id="{{ $prefix }}-volcano-plot" src='{{"/run-images?path=".urlencode("/$runHash/workingDir/Analysis/02_sgRNA-Ranking_Results/sgRNA_VolcanoPlots/$prefix"."_sgRNA_volcano.png" )}}'>
+@php
+	$_nonTStr = $extraData['nonT'] ? "_nonT" : "" ;
+@endphp
+
+<img id="{{ $prefix }}-volcano-plot" src='{{"/run-images?path=".urlencode("/$runHash/workingDir/Analysis/02_sgRNA-Ranking_Results/sgRNA_VolcanoPlots/$prefix"."_sgRNA_volcano$_nonTStr.png" )}}'>
