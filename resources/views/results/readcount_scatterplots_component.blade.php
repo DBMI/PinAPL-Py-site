5a81759192ed6080135197b9adfacb205ba09b25
DarrenChangSD <djc035@ucsd.edu>
2019-09-09 11:53:49 -0700

    added extraData var for nonT pre checked

--- resources/views/results/readcount_scatterplots_component.blade.php
+++ resources/views/results/readcount_scatterplots_component.blade.php
@@ -1,1 +1,5 @@
-<img id="{{ $prefix }}-readcount-scatterplot" src='{{"/run-images?path=".urlencode("/$runHash/workingDir/Analysis/ReadCount_Scatterplots/counts_$prefix.png" )}}'>
+@php
+	$_nonTStr = $extraData['nonT'] ? "_nonT" : "" ;
+@endphp
+
+<img id="{{ $prefix }}-readcount-scatterplot" src='{{"/run-images?path=".urlencode("/$runHash/workingDir/Analysis/02_sgRNA-Ranking_Results/sgRNA_Scatterplots/counts_$prefix"."_scatterplot$_nonTStr.png" )}}'>
