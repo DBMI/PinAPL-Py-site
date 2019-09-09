7b88a6b483e2e0ef0d5fd58db56925b919d7231d
DarrenChangSD <djc035@ucsd.edu>
2019-09-09 12:06:14 -0700

    zscore plot added extraData for nonT pre checked

--- resources/views/results/zscore_plots_component.blade.php
+++ resources/views/results/zscore_plots_component.blade.php
@@ -1,1 +1,5 @@
-<img id="{{ $prefix }}-zscore-plot" src='{{"/run-images?path=".urlencode("/$runHash/workingDir/Analysis/02_sgRNA-Ranking_Results/sgRNA_z-Scores/$prefix"."_sgRNA_zScores.png" )}}'> 
+@php
+	$_nonTStr = $extraData['nonT'] ? "_nonT" : "" ;
+@endphp
+
+<img id="{{ $prefix }}-zscore-plot" src='{{"/run-images?path=".urlencode("/$runHash/workingDir/Analysis/02_sgRNA-Ranking_Results/sgRNA_z-Scores/$prefix"."_sgRNA_zScores$_nonTStr.png" )}}'> 
