feece605744eb720c2350cb3e89bdc0590cc5abb
DarrenChangSD <djc035@ucsd.edu>
2019-09-09 11:59:37 -0700

    sgrna ranking results updated tab positions

--- resources/views/results/sgrna_ranking_results.blade.php
+++ resources/views/results/sgrna_ranking_results.blade.php
@@ -1,10 +1,13 @@
 <?php
 $sections = [
-	"sgrna_rankings" => "sgRNA Rankings",
-	"sgrna_scatter_plots"  => "Scatterplots",
-	"various_plots" => "Plots",
-	"readcount_pages" => "Read Count",
-	"heatmap" => "Heatmap",
+	"sgrna_rankings" => "Rankings",
+	"sgrna_plots"  => "Plots",
+	//"various_plots" => "Plots",
+	"pvalue_dist" => "p-Values",
+	"readcount_distribution" => "Read Count Distribution",
+	"heatmap" => "Clusters",
+	"replicate_correlation"	=> "Replicate Correlation",
+	"readcount_dispersion" => "Read Count Dispersion",
 ];
 ?>
 @extends('layouts.results_section_tabs', ['sections' => $sections, "selfName"=>"sgrna-ranking-results"])
