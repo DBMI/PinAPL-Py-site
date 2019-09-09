86dbd644154819cbf052308d1cca38deb34ee11d
DarrenChangSD <djc035@ucsd.edu>
2019-09-09 11:57:05 -0700

    read depth file pathing updated

--- resources/views/results/sequencing_depth_component.blade.php
+++ resources/views/results/sequencing_depth_component.blade.php
@@ -1,5 +1,5 @@
 <div class="row align-center">
 	<div class="column shrink">
-		<img src='{{"/run-images?path=".urlencode("/$runHash/workingDir/Analysis/Read_Depth/Read_Depth.png")}}'>
+		<img src='{{"/run-images?path=".urlencode("/$runHash/workingDir/Analysis/01_Alignment_Results/Read_Depth/Read_Depth.png")}}'>
 	</div>
 </div>
