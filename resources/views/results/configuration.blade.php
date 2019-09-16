832e4d4e451d23050521c26da7548bbe72119d0f
DarrenChangSD <djc035@ucsd.edu>
2019-09-16 01:23:52 -0700

    configuration page cut off using parser

--- resources/views/results/configuration.blade.php
+++ resources/views/results/configuration.blade.php
@@ -1,11 +1,28 @@
 <?php
 use Illuminate\Support\Facades\File;
 use Illuminate\Support\Str;
+use Symfony\Component\Yaml\Yaml;
+
+/*
+$dir = storage_path("/runs/$hash/workingDir");
+$data = Yaml::parseFile("$dir/Analysis/00_LogFile/configuration.yaml");
+$line_number = $data["PrintHighlights"];
+*/
+
+$search      = "PrintHighlights";
+$dir         = storage_path("/runs/$hash/workingDir");
+$lines       = file("$dir/Analysis/00_LogFile/configuration.yaml");
+$line_number = false;
+
+while (list($key, $line) = each($lines) and !$line_number) {
+   $line_number = (strpos($line, $search) !== FALSE) ? $key + 1 : $line_number;
+}
+
 $filePath = storage_path("/runs/$hash/workingDir/Analysis/00_LogFile/configuration.yaml");
 if (File::exists($filePath)){
 	$fileContents = File::get($filePath);
 	$fileContents = explode(PHP_EOL, $fileContents);
-	$fileContents = array_slice($fileContents, 0, 54);
+	$fileContents = array_slice($fileContents, 0, $line_number);
 	$fileContents = implode(PHP_EOL, $fileContents);
 }
 ?>
