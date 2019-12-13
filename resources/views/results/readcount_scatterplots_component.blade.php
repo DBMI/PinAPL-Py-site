18aa92dc859b88670fb1ce026de0d20ff061b42f
DarrenChangSD <ubuntu@ip-172-31-22-21.us-west-1.compute.internal>
2019-12-13 19:18:38 +0000

    Made the IDs flag show up when needed

--- resources/views/results/readcount_scatterplots_component.blade.php
+++ resources/views/results/readcount_scatterplots_component.blade.php
@@ -2,4 +2,13 @@
 	$_nonTStr = $extraData['nonT'] ? "_nonT" : "" ;
 @endphp
 
-<img id="{{ $prefix }}-readcount-scatterplot" src='{{"/run-images?path=".urlencode("/$runHash/workingDir/Analysis/02_sgRNA-Ranking_Results/sgRNA_Scatterplots/counts_$prefix"."_scatterplot_IDs$_nonTStr.png" )}}'>
+<?php
+use Symfony\Component\Yaml\Yaml;
+
+$dir = storage_path("/runs/$runHash/workingDir");
+$data = Yaml::parseFile("$dir/configuration.yaml");
+$boo = $data["scatter_annotate"];
+$_IDs = $boo ? "_IDs" : "" ;
+?>
+
+<img id="{{ $prefix }}-readcount-scatterplot" src='{{"/run-images?path=".urlencode("/$runHash/workingDir/Analysis/02_sgRNA-Ranking_Results/sgRNA_Scatterplots/counts_$prefix"."_scatterplot$_IDs$_nonTStr.png" )}}'>
--- resources/views/results/zscore_plots_component.blade.php
+++ resources/views/results/zscore_plots_component.blade.php
@@ -2,4 +2,13 @@
 	$_nonTStr = $extraData['nonT'] ? "_nonT" : "" ;
 @endphp
 
-<img id="{{ $prefix }}-zscore-plot" src='{{"/run-images?path=".urlencode("/$runHash/workingDir/Analysis/02_sgRNA-Ranking_Results/sgRNA_z-Scores/$prefix"."_sgRNA_zScores_IDs$_nonTStr.png" )}}'> 
+<?php
+use Symfony\Component\Yaml\Yaml;
+
+$dir = storage_path("/runs/$runHash/workingDir");
+$data = Yaml::parseFile("$dir/configuration.yaml");
+$boo = $data["scatter_annotate"];
+$_IDs = $boo ? "IDs" : "" ;
+?>
+
+<img id="{{ $prefix }}-zscore-plot" src='{{"/run-images?path=".urlencode("/$runHash/workingDir/Analysis/02_sgRNA-Ranking_Results/sgRNA_z-Scores/$prefix"."_sgRNA_zScores$_IDs$_nonTStr.png" )}}'> 
