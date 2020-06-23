3b59d1b18be654f2fc1d969bb108926aa9bcb994
Tyler Bath <tylerbath@gmail.com>
2020-06-22 23:18:08 -0700

    name problems

--- resources/views/results/zscore_plots_component.blade.php
+++ resources/views/results/zscore_plots_component.blade.php
@@ -8,7 +8,7 @@ use Symfony\Component\Yaml\Yaml;
 $dir = storage_path("/runs/$runHash/workingDir");
 $data = Yaml::parseFile("$dir/configuration.yaml");
 $boo = $data["scatter_annotate"];
-$_IDs = $boo ? "IDs" : "" ;
+$_IDs = $boo ? "_IDs" : "" ;
 ?>
 
 <img id="{{ $prefix }}-zscore-plot" src='{{"/run-images?path=".urlencode("/$runHash/workingDir/Analysis/02_sgRNA-Ranking_Results/sgRNA_z-Scores/$prefix"."_sgRNA_zScores$_IDs$_nonTStr.png" )}}'> 
